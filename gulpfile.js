140f419c1c2a2d08babb9ac4c0f45888fd3a10f4
AlexDrags <vera2052@mail.ru>
2021-12-11 17:00:27 +0300

    123

--- gulpfile.js
+++ gulpfile.js
@@ -1,114 +1,51 @@
-const gulp = require("gulp");
-const plumber = require("gulp-plumber"); //обработчик на ошибки
-const sourcemap = require("gulp-sourcemaps"); //строит карты кода от итогового css к less файлу где это писалось
-const less = require("gulp-less");
+const { src, dest, parallel, series, watch } = require('gulp');
+const browser = require('browser-sync').create();
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+const less = require('gulp-less');
 const postcss = require("gulp-postcss"); //библиотека префиксов
 const autoprefixer = require("autoprefixer"); //добавлять префиксы
 const csso = require("gulp-csso");
 const rename = require("gulp-rename");
-const imagemin = require("gulp-imagemin");
-const webp = require("gulp-webp");
-const svgstore = require("svg-store");
-const sync = require("browser-sync").create(); //поднятие локального серва 
-const build = require("gulp");
-const del = require("del");
 
-var paths = {
-    styles: {
-      src: 'source/CSS/**/*.less',
-      dest: 'build/CSS/'
-    },
-    scripts: {
-      src: 'source/scripts/**/*.js',
-      dest: 'build/scripts/'
-    }
-  }
+function browsersync() {
+    browser.init({
+        server: { baseDir: 'source/' },
+        notify: false,
+        online: true
+    })
+}
 
-// Styles
-const styles = () => {
-    return gulp.src("source/styles/style.less")
-    .pipe(plumber())
-    .pipe(sourcemap.init())
+function scripts() {
+    return src(['source/js/**/*.js'])
+    .pipe(concat('script.min.js'))
+    .pipe(uglify())
+    .pipe(dest('dist/scripts/'))
+    .pipe(browser.stream())
+}
+
+function styles() {
+    return src(['source/styles/style.less'])
     .pipe(less())
     .pipe(postcss([
         autoprefixer()
     ]))
     .pipe(csso())
-    .pipe(rename("style.min.css"))
-    .pipe(sourcemap.write("build/CSS"))
-    .pipe(gulp.dest("build/CSS"))
-    .pipe(sync.stream());
+    .pipe(rename('style.min.css'))
+    .pipe(dest('dist/CSS/'))
+    .pipe(browser.stream())
 }
 
-const images = () => {
-    return gulp.src("source/img/**/*.{jpg,png.svg}")
-        .pipe(imagemin([
-            imagemin.optipng({optimizationLevel: 3}),
-            imagemin.jpegtran({progressive: true}),
-            imagemin.svgo()
-        ]))
-        .pipe(gulp.dest("build/img"))
+function watcher() {
+    watch([
+        'source/**/*.js',
+        'source/*.html',
+        'source/**/*.less'
+    ],scripts, styles)
 }
 
-const webp = () => {
-    return gulp.src("source/img/**/*.{jpg.pmg}")
-        .pipe(webp({quality: 90}))
-        .pipe(gulp.dest("build/img"))
-}
-
-
-//Server 
-const server = (done) => {
-    sync.init({
-        server: {
-            baseDir: 'build'
-        },
-        
-        cors: true,
-        notify: false,
-        ui: false,
-    });
-    done();
-}
-
-
-//Watcher
-const watcher = () => {
-    gulp.watch("source/styles/**/*.less", gulp.series("styles"));
-    gulp.watch("source/*.html").on("change", sync.reload);
-}
-
-
-const copy = () => {
-    return gulp.src([
-        "source/fonts/**/*.{woff,woff2}",
-        "source/img/**",
-        "source/js/**"
-    ], {
-        base: "source"
-    })
-    .pipe(gulp.dest("build"));
-}
-
-
-const clean = () => {
-    return del("build");
-}
-
-
-const build = () => gulp.series(
-    "clean",
-    "copy",
-    "css",
-    "sprite",
-    "html"
-)
-
+exports.browsersync = browsersync;
+exports.scripts = scripts;
 exports.styles = styles;
-exports.images = images;
-exports.webp = webp;
-exports.server = server;
-exports.watcher = watcher;
-exports.copy = copy;
-exports.clean = clean;
-exports.build = build;
+
+exports.default = parallel(scripts, styles, browsersync, watcher);
---
+++ source/js/form.js
@@ -0,0 +1,3 @@
+let form = document.querySelector('form');
+let button = document.querySelector('button');
+let apt
